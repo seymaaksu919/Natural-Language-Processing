a6f6dea77b13f88c8b0d45a290442d0728acf5f9
Şeyma Aksu <110054400+seymaaksu919@users.noreply.github.com>
2025-07-10 13:50:12 +0300

    Add files via upload

---
+++ Tf-idf_spam.py
@@ -0,0 +1,24 @@
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
+
+df = pd.read_csv("/home/seymaaksu/spyder/sms_spam.csv")
+
+vectorizer = TfidfVectorizer()
+
+
+X=vectorizer.fit_transform(df["text"])
+
+
+#kelime kümesi alınır.
+
+feature_names = vectorizer.get_feature_names_out()
+
+tfidf_score = X.mean(axis=0).A1 # ortalama tf-idf degerleri
+
+
+df_tfidf = pd.DataFrame({"word":feature_names, "tfidf_score":tfidf_score})
+
+df_tfidf_sorted = df_tfidf.sort_values(by = "tfidf_score", ascending=False)
+
+print(df_tfidf_sorted)
---
+++ tf-ıdf.py
@@ -0,0 +1,39 @@
+#TF-IDF
+
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd 
+import numpy as np
+
+
+#Örnek belge
+
+documents = ["Kedi çok tatlı bir hayvandır." ,
+             "kedi ve köpekler çok tatlı hayvanlardır",
+             "Arılar bal üretirler"]
+
+
+
+tfidf_vectorizer= TfidfVectorizer()
+
+#Metinleri sayısala çeviririz.
+
+X = tfidf_vectorizer.fit_transform(documents)
+
+#kelime kümesi
+
+feature_names = tfidf_vectorizer.get_feature_names_out()
+
+
+print("TF-IDF Vektor temsilleri:")
+vektor_temsili = X.toarray()
+print(vektor_temsili)
+
+#DataFrame haline getirilir.
+df_tfidf = pd.DataFrame(vektor_temsili, columns = feature_names)
+
+
+#Sadece kedi sütunun ortalamasına bakılır.
+kedi_tfidf = df_tfidf["kedi"]
+kedi_mean_tfidf = kedi_tfidf.mean()
